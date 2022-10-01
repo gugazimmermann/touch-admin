f4fc6d23575749cb01163ea5c3321c9a62aa4a1b
Guga Zimmermann <gugazimmermann@gmail.com>
2022-10-01 00:46:05 -0300

    update profile type

--- lib/lambdas/common/types.ts
+++ lib/lambdas/common/types.ts
@@ -1,7 +1,18 @@
 export type ProfileType = {
   profileID: string;
   email: string;
+  phone?: string;
+  name?: string;
+  documenttype?: string;
+  document?: string;
+  zipCode?: string;
+  state?: string;
+  city?: string;
+  district?: string;
+  street?: string;
+  number?: string;
+  complement?: string;
+  avatar?: string;
   createdAt?: string;
   updatedAt?: string;
 }
-
