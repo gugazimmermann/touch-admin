36c3f1dea3063da156a289d48ecf356ca531e304
Guga Zimmermann <gugazimmermann@gmail.com>
2022-10-02 12:45:00 -0300

    enable logs

--- lib/constructs/restAPI.ts
+++ lib/constructs/restAPI.ts
@@ -1,9 +1,13 @@
+import { RemovalPolicy, Duration } from 'aws-cdk-lib';
 import {
+  AccessLogFormat,
   CognitoUserPoolsAuthorizer,
+  LogGroupLogDestination,
   MethodLoggingLevel,
   RestApi,
 } from "aws-cdk-lib/aws-apigateway";
 import { UserPool } from "aws-cdk-lib/aws-cognito";
+import { LogGroup, RetentionDays } from "aws-cdk-lib/aws-logs";
 import { Construct } from "constructs";
 
 type RestApiConstructProps = {
@@ -19,17 +23,26 @@ export class RestApiConstruct extends Construct {
   constructor(scope: Construct, id: string, props: RestApiConstructProps) {
     super(scope, id);
 
+    const restApiLogGroup = new LogGroup(scope, `${props.stackName}-restApiLogGroup-${props.stage}`, {
+      removalPolicy: RemovalPolicy.DESTROY,
+      retention: RetentionDays.ONE_MONTH
+    });
+
     this.restApi = new RestApi(
       scope,
       `${props.stackName}-RestApi-${props.stage}`,
       {
         description: `${props.stackName} RestApi ${props.stage}`,
+        cloudWatchRole: true,
+
         deployOptions: {
           stageName: "dev",
           dataTraceEnabled: true,
           tracingEnabled: true,
           metricsEnabled: true,
           loggingLevel: MethodLoggingLevel.INFO,
+          accessLogDestination: new LogGroupLogDestination(restApiLogGroup),
+          accessLogFormat: AccessLogFormat.jsonWithStandardFields(),
         },
         defaultCorsPreflightOptions: {
           allowHeaders: ["Content-Type",  "X-Amz-Date", "Authorization", "X-Api-Key"],
