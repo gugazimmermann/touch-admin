11f779963c98017c1710dba36b56a92f0222d990
Guga Zimmermann <gugazimmermann@gmail.com>
2022-10-03 18:48:15 -0300

    chage stage url

--- lib/constructs/common/cors.ts
+++ lib/constructs/common/cors.ts
@@ -1,6 +1,6 @@
 export const corsDomains = [
   "http://localhost:3000",
-  "https://d1aewi60iom71h.cloudfront.net/",
+  "https://d113tyd56o48tz.cloudfront.net/",
   "https://wwww.touchsistemas.com.br",
   "https://touchsistemas.com.br",
   "https://admin.touchsistemas.com.br",
