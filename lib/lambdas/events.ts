5a0d21e1d41b4bf926e213f2206912469b49fdb3
Guga Zimmermann <gugazimmermann@gmail.com>
2022-10-07 19:32:37 -0300

    events post and get

--- lib/lambdas/events.ts
+++ lib/lambdas/events.ts
@@ -6,7 +6,6 @@ import {
   APIGatewayProxyResult,
 } from "aws-lambda";
 import eventsPost from "./events/events-post";
-import eventsPut from "./events/events-put";
 import eventsGet from "./events/events-get";
 
 const TABLE_NAME = process.env.TABLE_NAME || "";
@@ -17,8 +16,6 @@ export const handler = async (event: APIGatewayEvent, context: Context): Promise
   console.debug(`event`, JSON.stringify(event, undefined, 2));
 
   if (event.httpMethod === "POST") return eventsPost(db, event, context.awsRequestId, TABLE_NAME);
-
-  if (event.httpMethod === "PUT") return eventsPut(db, event, context.awsRequestId, TABLE_NAME);
 
   return eventsGet(db, event, context.awsRequestId, TABLE_NAME);
 
--- lib/lambdas/events/events-get.ts
+++ lib/lambdas/events/events-get.ts
@@ -2,8 +2,8 @@ import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
 import commonResponse from "../common/commonResponse";
 import { DocumentClient } from "aws-sdk/lib/dynamodb/document_client";
 
-const getOne = async (db: DocumentClient, profileID: string, requestID: string, TableName: string): Promise<APIGatewayProxyResult> => {
-  const params = { TableName, Key: { profileID } };
+const getByEventID = async (db: DocumentClient, eventID: string, requestID: string, TableName: string): Promise<APIGatewayProxyResult> => {
+  const params = { TableName, Key: { eventID } };
   console.debug(`params`, JSON.stringify(params, undefined, 2));
   try {
     const res = await db.get(params).promise();
@@ -14,12 +14,18 @@ const getOne = async (db: DocumentClient, profileID: string, requestID: string, 
   }
 }
 
-const getAll = async (db: DocumentClient, requestID: string, TableName: string): Promise<APIGatewayProxyResult> => {
-  const params = { TableName };
+const getByProfileID = async (db: DocumentClient, profileID: string, requestID: string, TableName: string): Promise<APIGatewayProxyResult> => {
+  const params = {
+    TableName: TableName,
+    IndexName: "byProfileID",
+    KeyConditionExpression: "#profileID = :profileID",
+    ExpressionAttributeNames: { "#profileID": "profileID" },
+    ExpressionAttributeValues: { ":profileID": profileID },
+  };
   console.debug(`params`, JSON.stringify(params, undefined, 2));
   try {
-    const res = await db.scan(params).promise();
-    return commonResponse(200, JSON.stringify({ data: res.Items, requestID }));
+    const res = await db.query(params).promise();
+    return commonResponse(200, JSON.stringify({ data: res.Items || [], requestID }));
   } catch (error) {
     console.error(`error`, JSON.stringify(error, undefined, 2));
     return commonResponse(500, JSON.stringify({ error, requestID}));
@@ -27,9 +33,10 @@ const getAll = async (db: DocumentClient, requestID: string, TableName: string):
 }
 
 const profileGet = async (db: DocumentClient, event: APIGatewayEvent, requestID: string, TableName: string): Promise<APIGatewayProxyResult> => {
+  const eventID = event?.pathParameters && event.pathParameters?.eventID;
+  if (eventID) return getByEventID(db, eventID, requestID, TableName);
   const profileID = event?.pathParameters && event.pathParameters?.profileID;
-  if (profileID) return getOne(db, profileID, requestID, TableName);
-  return getAll(db, requestID, TableName);
+  return getByProfileID(db, profileID as string, requestID, TableName);
 };
 
 export default profileGet;
